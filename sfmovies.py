0fe21341a2b1f590001e63be062a7ddbe4f38031
Jyothi Madhavapeddy <jyothi@Jyothis-MacBook-Air.local>
2019-04-30 14:26:56 -0700

    Updated files for googlemapsAPI change

--- sfmovies.py
+++ sfmovies.py
@@ -3,6 +3,7 @@ import jinja2
 import json
 import model
 from model import Movie
+import sys
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
@@ -10,7 +11,10 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+    gmaps = {}
+    mapskeyvalue = app.config.get('mapskey')
+    gmaps['key'] = mapskeyvalue
+    return render_template("index.html", mapskey=gmaps)
 
 @app.route("/movies")
 def getmoviesfromdb():
@@ -19,20 +23,21 @@ def getmoviesfromdb():
     # make a dictionary of all the movies in the database. Each movie can map to many locations
     output = {}
     for movie in movie_list:
-    	if movie.title in output:
-    		output[movie.title].append(movie.location)
-    	else:
-    		output[movie.title] = [movie.location]
+        if movie.title in output:
+            output[movie.title].append(movie.location)
+        else:
+            output[movie.title] = [movie.location]
 
     # make an array of objects from the above dictionary to send to frontend view
     outlist = []
     for k,v in output.items():
-    	moviedict = {}
-    	moviedict['title'] = k
-    	moviedict['location'] = v
-    	outlist.append(moviedict)
+        moviedict = {}
+        moviedict['title'] = k
+        moviedict['location'] = v
+        outlist.append(moviedict)
 
     return jsonify(collection=outlist)    
     
 if __name__ == '__main__':
+    app.config['mapskey'] = sys.argv[1]
     app.run()
